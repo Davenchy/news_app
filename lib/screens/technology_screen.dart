24ff78311de40f69d2204e244b02bfb1a8f13098
Davenchy <firon1222@gmail.com>
2021-07-30 18:04:12 +0200

    update tech screen

--- lib/screens/technology_screen.dart
+++ lib/screens/technology_screen.dart
@@ -1,33 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/models/news_article.dart';
-import 'package:news_app/widgets/article_item.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubits/app_cubit.dart';
+import 'package:news_app/widgets/articles_list.dart';
 
 class TechnologyScreen extends StatelessWidget {
   const TechnologyScreen({Key? key}) : super(key: key);
 
-  static const String image =
-      'https://cdn.abcotvs.com/dip/images/10918729_073021-wls-cook-mask-jess430a-vid.jpg?w=1600';
-
   @override
   Widget build(BuildContext context) {
-    return _buildArticlesList(context);
-  }
-
-  Widget _buildArticlesList(BuildContext context) {
-    return ListView.separated(
-      physics: const BouncingScrollPhysics(),
-      padding: const EdgeInsets.all(16.0),
-      itemCount: 10,
-      separatorBuilder: (_, __) => SizedBox(height: 16.0),
-      itemBuilder: (context, index) {
-        final article = NewsArticle(
-          title: 'Article Title',
-          imgUrl: image,
-          url: '',
-          publishedAt: DateTime.now(),
+    return BlocBuilder<AppCubit, AppState>(
+      buildWhen: (p, current) =>
+          current is AppFetchingArticlesState ||
+          current is AppViewArticlesState,
+      builder: (context, state) {
+        final cubit = AppCubit.of(context);
+        return ArticleList(
+          articles: cubit.techArticles,
+          onRefresh: cubit.fetchTechArticles,
+          isFetching: state is AppFetchingArticlesState,
         );
-
-        return ArticleItem(article);
       },
     );
   }
