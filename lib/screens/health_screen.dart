b5790c1b000982d791ccb6f27a75daac45a0c28d
Davenchy <firon1222@gmail.com>
2021-07-30 16:07:17 +0200

    add health and science screens

---
+++ lib/screens/health_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScienceScreen extends StatelessWidget {
+  const ScienceScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Health Screen'),
+    );
+  }
+}
