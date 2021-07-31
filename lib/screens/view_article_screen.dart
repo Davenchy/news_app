0ceadbf7e2ac7db5f27b660839f1afa5f16a4f83
Davenchy <firon1222@gmail.com>
2021-07-31 11:53:00 +0200

    add page loading progress to view artice screen fix webview android build issue

--- lib/screens/view_article_screen.dart
+++ lib/screens/view_article_screen.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ViewArticleScreen extends StatelessWidget {
+class ViewArticleScreen extends StatefulWidget {
   const ViewArticleScreen(this.url, {Key? key}) : super(key: key);
   final String url;
 
   @override
+  _ViewArticleScreenState createState() => _ViewArticleScreenState();
+}
+
+class _ViewArticleScreenState extends State<ViewArticleScreen> {
+  bool showIndicator = false;
+  double? indicatorProgress;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        bottom: showIndicator
+            ? PreferredSize(
+                child: LinearProgressIndicator(
+                  value: indicatorProgress,
+                ),
+                preferredSize: Size.fromHeight(10),
+              )
+            : null,
+      ),
       body: WebView(
-        initialUrl: url,
+        initialUrl: widget.url,
+        onProgress: (int progress) {
+          setState(() {
+            indicatorProgress = progress / 100;
+          });
+        },
+        onPageStarted: (String thing) {
+          setState(() {
+            showIndicator = true;
+          });
+        },
+        onPageFinished: (String thing) {
+          setState(() {
+            showIndicator = false;
+          });
+        },
       ),
     );
   }
